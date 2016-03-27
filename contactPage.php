56876e34a9d029c2b2b4c90454dad75c6a3ba9cc
thyjesster <firenzanerd@gmail.com>
2016-03-26 21:22:12 -0400

    Update contactPage.php

--- contactPage.php
+++ contactPage.php
@@ -1,10 +1,11 @@
 <?php
-// define variables and set to empty values
-$name = $email = $comment = $website = "";
+//trims and removes special characters
+$firstname = $lastname = $email = $subject = $message = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   $firstname = test_input($_POST["name"]);
-   $lastname = test_input($_POST["email"]);
+   $firstname = test_input($_POST["firstname"]);
+   $lastname = test_input($_POST["lastname"]);
+   $email = test_input($_POST["email"]);
    $subject = test_input($_POST["website"]);
    $message = test_input($_POST["comment"]);
 }
