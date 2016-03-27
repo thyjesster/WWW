d59f97e20531803384d28eea1ea74b03d81e2e3f
thyjesster <firenzanerd@gmail.com>
2016-03-26 20:33:49 -0400

    Update contactPage.php

--- contactPage.php
+++ contactPage.php
@@ -1,1 +1,18 @@
+<?php
+// define variables and set to empty values
+$name = $email = $comment = $website = "";
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   $firstname = test_input($_POST["name"]);
+   $lastname = test_input($_POST["email"]);
+   $subject = test_input($_POST["website"]);
+   $message = test_input($_POST["comment"]);
+}
+
+function test_input($data) {
+   $data = trim($data);
+   $data = stripslashes($data);
+   $data = htmlspecialchars($data);
+   return $data;
+}
+?>
