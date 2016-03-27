43199eb59c1b38db3a5424b8ff703b4cdf739532
Baron <sanmuga@uwindsor.ca>
2016-03-27 16:44:23 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,80 @@
 <html>
-  <head>
-    <title>TITLE</title>
-  </head>
-  <body>
-    <p>Baron Wasteland</p>
-  <?php
-  ?>
-  </body>
+
+<head>
+    <title>BlogLife</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- This is how you comment?-->
+</head>
+
+<body>
+
+<div class="blog-masthead">
+    <nav class="navbar navbar-dark bg-inverse">
+        <a class="navbar-brand"></a>
+        <ul class="nav navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">
+                    Home
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="logIn.php">
+                    Log In
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contact.php">
+                    Contact
+                </a>
+            </li>
+
+        </ul>
+
+    </nav>
+
+</div>
+
+<div class="jumbotron" style="background-color: lightsteelblue">
+    <div class="container">
+        <h1 class="blog-title">Windsor Bloglife</h1>
+        <p class="lead blog-description">
+            A blog about the life of a University of Windsor student!
+            How cool!
+        </p>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-xl-8 col-lg-8 col-sm-8 blog-main" style="border: medium">
+            <h2 class="blog-post-title">Blog Post!</h2>
+            <p class="blog-post-meta">
+                March 27, 2016 by
+                <a href="#">John Doe</a>
+            </p>
+            <p> Today's class in databases was awesome! I had a great time!!!
+                I can't wait until the final!!<strong> /s</strong></p>
+            <hr>
+            <p>
+                Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+                mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis
+                vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus
+                sit amet fermentum.
+            </p>
+        </div>
+        <div class="col-xl-3 col-lg-3 col-sm-3 col-sm-offset-1 blog-sidebar" style="background-color: beige">
+            <h4>About section</h4>
+            <p>Words stuff..... lots of interesting stuff to see over here hahaah</p>
+        </div>
+    </div>
+
+
+</div>
+<script src="https://code.jquery.com/jquery-2.2.2.min.js" integrity="sha256-36cp2Co+/62rEAAYHLmRCPIych47CvdM+uTBJwSzWjI=" crossorigin="anonymous"></script>
+<script src="js/bootstrap.min.js"> </script>
+</body>
+
 </html>
