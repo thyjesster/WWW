59201365b30f69567e94161f66af340412dd20af
Baron <sanmuga@uwindsor.ca>
2016-03-27 19:48:15 -0400

    Update login.php

--- login.php
+++ login.php
@@ -3,6 +3,14 @@
     <title>Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <style>
+        .jumbotron {
+            height: 200px;
+        }
+    </style>
+
+
 </head>
 <body>
 
@@ -32,6 +40,16 @@
 
 </div>
 
+<div class="jumbotron" style="background-color: lightsteelblue">
+    <div class="container">
+        <h1 class="blog-title">Windsor Bloglife</h1>
+        <p class="lead blog-description">
+            A blog about the life of a University of Windsor student!
+            How cool!
+        </p>
+    </div>
+</div>
+
 <!--
 <form action="loginAttempt.php" method="post">
     User Name:<br>
@@ -42,28 +60,32 @@
     <input type="Login">
 </form>
 -->
+<d
+
 <div class="container-fluid" >
 <div class="row">
-    <div class="container col-lg-6 col-lg-offset-3 text-lg-center " >
+    <div class="container col-lg-6 col-lg-offset-3 text-lg-center ">
+        <h2 class="form-signin-heading display-3">Please sign in</h2>
+        <div class="form-group">
 
-        <form class="form-signin">
-            <h2 class="form-signin-heading display-3">Please sign in</h2>
-            <label for="inputEmail" class="sr-only">Email address</label>
-            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
-            <label for="inputPassword" class="sr-only">Password</label>
-            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox" value="remember-me"> Remember me
-                </label>
-            </div>
+            <form action="welcome.php" method="post" class="form-signin">
+                    <input type="text" class="form-control" placeholder="Username">
+
+        </div>
+        <div class=" form-group">
+
+                     <input type="text" class="form-control" placeholder="Password">
+
+        </div>
+        <div>
             <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-        </form>
+            </form>
+        </div>
+
+        </div>
     </div>
 </div>
 </div>
-<a href="index.php">HOMEPAGE THO</a> /*link to home page*/
-<a href="contact.php">CONTACT US</a> /*link to contact page*/
 
 
 <script src="https://code.jquery.com/jquery-2.2.2.min.js" integrity="sha256-36cp2Co+/62rEAAYHLmRCPIych47CvdM+uTBJwSzWjI=" crossorigin="anonymous"></script>
