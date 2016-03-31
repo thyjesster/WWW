927178bdb358592090c58c98d3a89d5bae66b866
thyjesster <firenzanerd@gmail.com>
2016-03-31 18:36:26 -0400

    Create TESTCONTACTPOST.php

---
+++ TESTCONTACTPOST.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<?php
+
+echo $_POST["firstname"];
+<br>
+echo $_POST["lastname"];
+<br>
+echo $_POST["email"];
+<br>
+echo $_POST["subject"];
+<br>
+echo $_POST["message"];
+<br>
+
+?>
+</body>
+<html>
